8f4305b8f9c1eb4cef956017c4de91e43ddf8987
Valerii Iatsko <dwr@codingbox.io>
2016-09-02 21:16:43 +0200

    server.js: eslint fix, also added .eslintrc.js file

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
+
 const app = express();
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
